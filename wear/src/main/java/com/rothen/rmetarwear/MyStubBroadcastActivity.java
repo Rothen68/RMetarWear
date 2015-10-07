22c267810b60040d6a10f7bb6a99a2b45ec97241
Rothen68 <rothen68@gmail.com>
2015-10-07 15:55:14 +0200

    Initial commit

---
+++ wear/src/main/java/com/rothen/rmetarwear/MyStubBroadcastActivity.java
@@ -0,0 +1,21 @@
+package com.rothen.rmetarwear;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Example shell activity which simply broadcasts to our receiver and exits.
+ */
+public class MyStubBroadcastActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent();
+        i.setAction("com.rothen.rmetarwear.SHOW_NOTIFICATION");
+        i.putExtra(MyPostNotificationReceiver.CONTENT_KEY, getString(R.string.title));
+        sendBroadcast(i);
+        finish();
+    }
+}
